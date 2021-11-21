c481107b12b2e806bd83d39f9bc0f02d978c5c24
aurora <1579598393@qq.com>
2021-11-21 13:36:11 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,21 @@
 //每次调用$.get()或$.post或$.ajax()的时候，会先调用ajaxPrefilter这个函数
 //在这个函数中，可以拿到我们给ajax提供的配置对象
+//options就表示$.get()或$.post或$.ajax()这个对象
 $.ajaxPrefilter(function(options) {
     console.log(options.url)
         //在发起真正的ajax请求之前统一拼接请求的根路径
-    options.url = 'http://ajax.fronted.itheima.net' + options.url
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 })
+
+options.complete = function(res) {
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+
+    }
+}
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+$(function() {
+    getUserInfo()
+    var layer = layui.layer
+    $('#btnLOgout').on('click', function() {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // 以/my开头的路径，需要在请求头中携带Authorization才能正常访问
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            //渲染用户头像
+            renderAvatar(res.data)
+        },
+        complete: function(res) {
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                localStorage.removeItem('token')
+                location.href = '/login.html'
+            }
+        }
+    })
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+            //name[0]表示name的第一个字符
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
